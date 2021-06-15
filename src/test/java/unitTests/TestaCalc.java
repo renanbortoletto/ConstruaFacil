547de05f1487e6046c85773462b9de26e590f40f
renanbortoletto <85020822+renanbortoletto@users.noreply.github.com>
2021-06-15 16:53:08 -0300

    Foi criado o menu da calculadora no metodo MAIN, a chamada do somarDoisNumeros e o seu teste unitario. 15/06/2021 - Foi criado teste unitario para cada tipo de conta (Adiçao, subt, multip e divisao). Obs: Error Infinity no testes de divisao linha 115

--- src/test/java/unitTests/TestaCalc.java
+++ src/test/java/unitTests/TestaCalc.java
@@ -21,6 +21,21 @@ public class TestaCalc {
         assertEquals(resultadoEsperado,resultadoAtual);
     }
 
+    @Test
+    public static void testarSomarDoisNumerosERROR() {
+        //1- Prepara - Configura - Given
+        int num1 = 5;
+        int num2 = 7;
+        int resultadoEsperado = 21;
+
+        //2 - Executa - When
+        int resultadoAtual = Calc.somarDoisNumeros(num1, num2);
+
+        //3 - Validar - Then
+        System.out.println("O resultado Esperado e " + resultadoEsperado + "\n O resultado Atual foi " + resultadoAtual);
+        assertEquals(resultadoEsperado,resultadoAtual);
+    }
+
 
     @Test
     public static void testarSubtrairDoisNumeros() {
@@ -36,6 +51,19 @@ public class TestaCalc {
         }
 
     @Test
+    public static void testarSubtrairDoisNumerosERROR() {
+
+        int num1 = 4;
+        int num2 = 1;
+        int resultadoEsperado = 32;
+
+        int resultadoAtual = Calc.subtrairDoisNumeros(num1, num2);
+
+        System.out.println("O resultado Esperado e " + resultadoEsperado + "\n O resultado Atual foi " + resultadoAtual);
+        assertEquals(resultadoEsperado,resultadoAtual);
+    }
+
+    @Test
     public static void testarMultiplicarDoisNumeros() {
 
         int num1 = 2;
@@ -48,13 +76,49 @@ public class TestaCalc {
     }
 
     @Test
+    public static void testarMultiplicarDoisNumerosERROR() {
+
+        int num1 = 2;
+        int num2 = 2;
+        int resultadoEsperado = 31;
+
+        int resultadoAtual = Calc.multiplicarDoisNumeros(num1, num2);
+        System.out.println("O resultado Esperado e " + resultadoEsperado + "\n O resultado Atual foi " + resultadoAtual);
+        assertEquals(resultadoEsperado,resultadoAtual);
+    }
+
+    @Test
     public static void testarDividirDoisNumeros() {
 
-        double num1 = 0;
+        double num1 = 10;
+        double num2 = 5;
+        double resultadoEsperado = 2;
+
+            double resultadoAtual = Calc.dividirDoisNumeros(num1, num2);
+        System.out.println("O resultado Esperado e " + resultadoEsperado + "\n O resultado Atual foi " + resultadoAtual);
+        assertEquals(resultadoEsperado,resultadoAtual);
+    }
+
+    @Test
+    public static void testarDividirDoisNumerosERROR_1() {
+
+        double num1 = 10;
+        double num2 = 5;
+        double resultadoEsperado = 0;
+
+        double resultadoAtual = Calc.dividirDoisNumeros(num1, num2);
+        System.out.println("O resultado Esperado e " + resultadoEsperado + "\n O resultado Atual foi " + resultadoAtual);
+        assertEquals(resultadoEsperado,resultadoAtual);
+    }
+
+    @Test
+    public static void testarDividirDoisNumerosERROR_2_Resultado_Infinity() {
+
+        double num1 = 1;
         double num2 = 0;
         double resultadoEsperado = 0;
 
-            double resultadoAtual = Calc.dividirDoisNumeros(num1, num2);
+        double resultadoAtual = Calc.dividirDoisNumeros(num1, num2);
         System.out.println("O resultado Esperado e " + resultadoEsperado + "\n O resultado Atual foi " + resultadoAtual);
         assertEquals(resultadoEsperado,resultadoAtual);
     }
